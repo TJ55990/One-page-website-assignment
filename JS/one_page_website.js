648312012b3d2565d4e896b219533766135c252b
TJ55990 <puneetsandhu.mail011@gmail.com>
2026-04-01 17:35:04 -0400

    Update one_page_website.js

--- JS/one_page_website.js
+++ JS/one_page_website.js
@@ -1,8 +1,18 @@
 function openModal(imageSrc) {
-    document.getElementById("lightbox").style.display = "block";
-    document.getElementById("lightbox-img").src = imageSrc;
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    lightbox.style.display = "flex";
+    lightboxImg.src = imageSrc;
 }
 
 function closeModal() {
     document.getElementById("lightbox").style.display = "none";
 }
+
+// Close when pressing ESC key
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
