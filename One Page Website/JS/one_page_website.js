f03c29c53bab721f542144cf55d2afd44f4add50
TJ55990 <puneetsandhu.mail011@gmail.com>
2026-04-01 14:03:52 -0400

    Delete One Page Website directory

--- One Page Website/JS/one_page_website.js
+++
@@ -1,8 +0,0 @@
-function openModal(imageSrc) {
-    document.getElementById("lightbox").style.display = "block";
-    document.getElementById("lightbox-img").src = imageSrc;
-}
-
-function closeModal() {
-    document.getElementById("lightbox").style.display = "none";
-}
